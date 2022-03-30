7ed846064cadfe56b5377a4c968fb496c19eebee
Mario H. Adaniya (mhadaniya) <mhadaniya@gmail.com>
2022-03-30 14:58:03 -0300

    :wrench: adding nuxt.config file and config meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,29 @@
 <template>
-    <div>hello world</div>
+    <div>
+        <div v-for="home in homes" :key="home.objectID" style="float: left;margin: 10px;">
+            <home-card :home="home" />
+        </div>
+    </div>
 </template>
+
+<script>
+import homes from '~/data/homes.json'
+
+export default {
+    head() {
+        return {
+            title: "Homepage",
+            meta: [{
+                name: 'description',
+                content: 'This is a homepage!',
+                hid: 'description'
+            }]
+        }
+    },
+    data() {
+        return {
+            homes: homes.slice(0,3)
+        }
+    }
+}
+</script>
